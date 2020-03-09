4aa7f2dada2f230a4388fc6edc18b555b7b95b9d
fstorck <fstorck@users.noreply.github.com>
2020-03-09 11:50:47 +0100

    CHG: - added sfc /scannow

--- batch/SystemCleanup/Windows-CleanupWIM.bat
+++ batch/SystemCleanup/Windows-CleanupWIM.bat
@@ -1,5 +1,10 @@
+sfc /scannow
+
 DISM /Online /Cleanup-Image /StartComponentCleanup
 DISM /Online /Cleanup-Image /StartComponentCleanup /ResetBase
 DISM /Online /Cleanup-Image /SPSuperseded
+
+cleanmrg
+
 
 
