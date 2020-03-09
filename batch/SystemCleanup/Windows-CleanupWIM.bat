4d9761ce4dbd8e6c2ad6db109b94566872e36c21
fstorck <fstorck@users.noreply.github.com>
2020-03-09 12:11:57 +0100

    CHG: added CheckHealth

--- batch/SystemCleanup/Windows-CleanupWIM.bat
+++ batch/SystemCleanup/Windows-CleanupWIM.bat
@@ -1,5 +1,5 @@
 sfc /scannow
-
+DISM /Online /Cleanup-Image /CheckHealth
 DISM /Online /Cleanup-Image /StartComponentCleanup
 DISM /Online /Cleanup-Image /StartComponentCleanup /ResetBase
 DISM /Online /Cleanup-Image /SPSuperseded
